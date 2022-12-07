cb64e35b3888777d3443c3e1515d63b4893899e0
Christopher Young <christopheryoung@MacBook123.hsd1.ca.comcast.net>
2022-12-06 17:36:47 -0800

    updated user routes

--- routes/user.js
+++ routes/user.js
@@ -3,20 +3,33 @@ const userRoutes = express.Router();
 // Models
 const User = require("../models/userModel");
 
+
+userRoutes.route("/api/user")
+//----- Retrieve all users
+.get((req, res) => {
+  User.find({})
+  .then(users => {
+    res.json({
+      success: true,
+      users
+    });
+  })
+  .catch(err => console.log(err));
+})
 //----- Retrieve single user
-userRoutes.post("/api/user", (req, res) => {
+.post((req, res) => {
   User.findById(req.body.id)
   .then(user => {
     if(user) {
       res.json({
         success: true,
         user
-      })
+      });
     } else {
       res.json({
         success: false,
         message: "User not found"
-      })
+      });
     }
   })
   .catch(err => console.log(err));
